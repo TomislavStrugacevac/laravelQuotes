68d9f7f02d603e0d9090198237362b04c244a432
Tomislav Strugačevac <tomislav.strugacevac@gmail.com>
2017-08-18 22:30:16 +0200

    update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+
+@section('title')
+    Trending quotes
+@endsection
+
+@section('styles')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+@endsection
+
+@section('content')
+
+    <section class="quotes">
+        <h1>Latest Quotes</h1>
+        <article class="quote">
+            <div class="delete"><a href="#">x</a></div>
+            Quote text
+            <div class="info">Created by <a href="#">Struga</a> created on...</div>
+        </article>
+        Pagination
+    </section>
+    <section class="edit-quote">
+        <h1>Add a Quote</h1>
+
+        <form action="">
+            <div class="input-group">
+                <label for="author">Your Name</label>
+                <input type="text" name="author" id="author" placeholder="Your Name"/>
+            </div>
+            <div class="input-group">
+                <label for="quote">Your Quote</label>
+                <textarea name="quote" id="quote" rows="5" placeholder="Your Quote"></textarea>
+            </div>
+            <button type="submit" class="btn">Submit Quote</button>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        </form>
+    </section>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!docype html>
+<html lang="hr">
+    <head>
+        <meta charset="utf8mb4">
+        <title> @yield('title')</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ URL::to('css/style.css') }}">
+        @yield('styles')
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    </head>
+
+<body>
+    <div class="container">
+
+        <div class="main">
+            @yield('content')
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+   @yield('scripts')
+</body>
+</html>
